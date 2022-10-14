ac2f94774a55c2400b7bac70246a389b4ce28155
anonimak <sanonimak@gmail.com>
2022-10-14 22:04:12 +0700

    feat: when clock clicked get random image & quote

--- app.js
+++ app.js
@@ -1,78 +1,90 @@
-const randomRng = Math.floor(Math.random() * 1000) + 0;
-document.body.style.cssText = `background: linear-gradient(to bottom, #00000099, transparent, #00000099),url(https://picsum.photos/seed/${randomRng}/1920/1080)`;
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundAttachment = "fixed"; 
+document.body.style.cssText = `background: linear-gradient(to bottom, #00000099, transparent, #00000099)`;
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundAttachment = 'fixed';
 
 // bgSwitch(bgImg), 1000;
+function getRandomRng() {
+  return Math.floor(Math.random() * 1000) + 0;
+}
+
+getRandomImage(getRandomRng());
+
+function getRandomImage(randomRng) {
+  document.body.style.backgroundImage = `url('https://picsum.photos/seed/${randomRng}/1920/1080')`;
+}
 
 //CLOCK
 let is24h = false;
 function clock() {
   const d = new Date();
-  let h = d.getHours().toString().padStart(2, "0");
-  let m = d.getMinutes().toString().padStart(2, "0");
-  let s = d.getSeconds().toString().padStart(2, "0");
-  let session = "";
+  let h = d.getHours().toString().padStart(2, '0');
+  let m = d.getMinutes().toString().padStart(2, '0');
+  let s = d.getSeconds().toString().padStart(2, '0');
+  let session = '';
   if (!is24h) {
     if (h >= 12) {
-      h = (h - 12).toString().padStart(2, "0");
-      session = "PM";
+      h = (h - 12).toString().padStart(2, '0');
+      session = 'PM';
     } else {
-      session = "AM";
+      session = 'AM';
     }
   }
-  document.querySelector("time").innerHTML = h + ":" + m + ":" + s;
-  document.querySelector(".ampm").innerHTML = session;
+  document.querySelector('time').innerHTML = h + ':' + m + ':' + s;
+  document.querySelector('.ampm').innerHTML = session;
 }
-document.querySelector(".clock").addEventListener("click", function () {
+document.querySelector('.clock').addEventListener('click', function () {
   is24h = !is24h;
+  // get random Image
+  getRandomImage(getRandomRng());
+  // get quote
+  updateQuote();
 });
 setInterval(clock, 1000);
 
 // Quotes
-const quote = document.querySelector("q");
-const cite = document.querySelector("cite");
+const quote = document.querySelector('q');
+const cite = document.querySelector('cite');
 
 updateQuote();
 
 // API -> https://github.com/lukePeavey/quotable
 async function updateQuote() {
-  const response = await fetch("https://api.quotable.io/random");
+  const response = await fetch('https://api.quotable.io/random');
   const data = await response.json();
   if (response.ok) {
     // Update DOM elements
     quote.innerText = data.content;
-    cite.textContent = "~ " + data.author + " ~";
+    cite.textContent = '~ ' + data.author + ' ~';
   } else {
-    quote.textContent = "An error occured";
+    quote.textContent = 'An error occured';
     console.log(data);
   }
 }
 
 // Timer
-const btnTimer = document.querySelector(".timer-button");
-const btnClock = document.querySelector(".clock-button");
-const divClock = document.querySelector(".clock");
-const divQuote = document.querySelector(".quote");
-const divTimerDisplay = document.querySelector(".timer-display");
-btnTimer.addEventListener("click", function () {
+const btnTimer = document.querySelector('.timer-button');
+const btnClock = document.querySelector('.clock-button');
+const divClock = document.querySelector('.clock');
+const divQuote = document.querySelector('.quote');
+const divTimerDisplay = document.querySelector('.timer-display');
+btnTimer.addEventListener('click', function () {
   // ask for permission to send notification
   Notification.requestPermission().then((perm) => {
-    isNotificationAllowed = perm === "granted";
+    isNotificationAllowed = perm === 'granted';
   });
-  divClock.classList.add("hidden");
-  divQuote.classList.add("hidden");
-  divTimerDisplay.classList.remove("hidden");
-  btnClock.classList.remove("hidden");
-  btnTimer.classList.add("hidden");
+  divClock.classList.add('hidden');
+  divQuote.classList.add('hidden');
+  divTimerDisplay.classList.remove('hidden');
+  btnClock.classList.remove('hidden');
+  btnTimer.classList.add('hidden');
 });
-btnClock.addEventListener("click", function () {
-  divClock.classList.remove("hidden");
-  divQuote.classList.remove("hidden");
-  divTimerDisplay.classList.add("hidden");
-  btnClock.classList.add("hidden");
-  btnTimer.classList.remove("hidden");
+btnClock.addEventListener('click', function () {
+  divClock.classList.remove('hidden');
+  divQuote.classList.remove('hidden');
+  divTimerDisplay.classList.add('hidden');
+  btnClock.classList.add('hidden');
+  btnTimer.classList.remove('hidden');
 });
 
 let inputHour = 0;
@@ -80,36 +92,36 @@ let inputMin = 0;
 let inputSec = 0;
 let time = 0; //Time is seconds.
 let isNotificationAllowed = false; // If notification is allowed.
-const btnInputHour = document.querySelector(".input-hour");
-btnInputHour.addEventListener("change", function (event) {
-  if (event.target.value === "1") {
-    document.querySelector(".displayhour").innerHTML = "hour";
+const btnInputHour = document.querySelector('.input-hour');
+btnInputHour.addEventListener('change', function (event) {
+  if (event.target.value === '1') {
+    document.querySelector('.displayhour').innerHTML = 'hour';
   }
 
   inputHour = Number(event.target.value);
   time = time + inputHour * 3600;
 });
-const btnInputMin = document.querySelector(".input-min");
-btnInputMin.addEventListener("change", function (event) {
+const btnInputMin = document.querySelector('.input-min');
+btnInputMin.addEventListener('change', function (event) {
   inputMin = Number(event.target.value);
   time = time + inputMin * 60;
 });
-const btnInputSec = document.querySelector(".input-sec");
-btnInputSec.addEventListener("change", function (event) {
+const btnInputSec = document.querySelector('.input-sec');
+btnInputSec.addEventListener('change', function (event) {
   inputSec = Number(event.target.value);
   time = time + inputSec;
 });
 
 let timer;
-const timerContainer = document.querySelector(".timer-container");
+const timerContainer = document.querySelector('.timer-container');
 const timerContainerCountdown = document.querySelector(
-  ".timer-container__countdown"
+  '.timer-container__countdown'
 );
 // Creating executable function
 const timerFunc = function () {
-  const hour = String(Math.trunc(time / 3600)).padStart(2, "0");
-  const min = String(Math.trunc((time / 60) % 60)).padStart(2, "0");
-  const sec = String(Math.trunc(time % 60)).padStart(2, "0");
+  const hour = String(Math.trunc(time / 3600)).padStart(2, '0');
+  const min = String(Math.trunc((time / 60) % 60)).padStart(2, '0');
+  const sec = String(Math.trunc(time % 60)).padStart(2, '0');
   // Displaying the timer
   timerContainerCountdown.innerText = `${hour}:${min}:${sec}`;
 
@@ -117,41 +129,41 @@ const timerFunc = function () {
   if (time === 0) {
     // Show notification
     if (isNotificationAllowed)
-      new Notification("Timer", { body: "Time is up!", icon: "/clock.png" });
+      new Notification('Timer', { body: 'Time is up!', icon: '/clock.png' });
     clearInterval(timer);
     btnInputHour.value = 0;
     btnInputMin.value = 0;
     btnInputSec.value = 0;
     time = 0;
-    btnStop.classList.add("hidden");
-    btnStart.classList.remove("hidden");
-    timerContainerCountdown.classList.add("hidden");
-    timerContainer.classList.remove("hidden");
+    btnStop.classList.add('hidden');
+    btnStart.classList.remove('hidden');
+    timerContainerCountdown.classList.add('hidden');
+    timerContainer.classList.remove('hidden');
   }
   time--;
 };
 
-const btnStart = document.querySelector(".start");
-const btnStop = document.querySelector(".stop");
-btnStart.addEventListener("click", function () {
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
+btnStart.addEventListener('click', function () {
   if (time > 0) {
     timerFunc();
     timer = setInterval(timerFunc, 1000);
-    btnStop.classList.remove("hidden");
-    btnStart.classList.add("hidden");
-    timerContainerCountdown.classList.remove("hidden");
-    timerContainer.classList.add("hidden");
+    btnStop.classList.remove('hidden');
+    btnStart.classList.add('hidden');
+    timerContainerCountdown.classList.remove('hidden');
+    timerContainer.classList.add('hidden');
   }
 });
-const selecElement = document.querySelectorAll(".input");
-btnStop.addEventListener("click", function () {
+const selecElement = document.querySelectorAll('.input');
+btnStop.addEventListener('click', function () {
   clearInterval(timer);
   btnInputHour.value = 0;
   btnInputMin.value = 0;
   btnInputSec.value = 0;
   time = 0;
-  btnStop.classList.add("hidden");
-  btnStart.classList.remove("hidden");
-  timerContainerCountdown.classList.add("hidden");
-  timerContainer.classList.remove("hidden");
+  btnStop.classList.add('hidden');
+  btnStart.classList.remove('hidden');
+  timerContainerCountdown.classList.add('hidden');
+  timerContainer.classList.remove('hidden');
 });
